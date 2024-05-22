2d4818894d98b87dbf7c9030cf44c44a9e19d201
sheri6186 <sheri.6186@gmail.com>
2024-05-22 22:49:59 +0500

    comments schema added

---
+++ backend/src/api/article-comment/routes/article-comment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article-comment router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::article-comment.article-comment');
